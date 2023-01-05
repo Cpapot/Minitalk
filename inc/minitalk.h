731146ebb2f591f5b3ba07c4344f6b9dbefec734
Corentin Papot <cpapot@z4r8p1.42lyon.fr>
2023-01-05 19:02:28 +0100

    SIGUSR ADDED

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -6,15 +6,17 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:32:06 by cpapot            #+#    #+#             */
-/*   Updated: 2023/01/04 17:09:09 by cpapot           ###   ########.fr       */
+/*   Updated: 2023/01/05 13:54:12 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 
 # define MINITALK_H
+
 # include <signal.h>
 # include "../libft/includes/libft.h"
+
 /*						Client						*/
 /*					check_error				*/
 void	check_args(int argc, char **argv);
--- src/server/server.c
+++ src/server/server.c
@@ -6,15 +6,27 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:33:11 by cpapot            #+#    #+#             */
-/*   Updated: 2023/01/04 16:53:21 by cpapot           ###   ########.fr       */
+/*   Updated: 2023/01/05 18:47:34 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minitalk.h"
 
+void	handler(int sig)
+{
+	ft_printf("Signal %d recu.\n", sig);
+}
+
 int	main(void)
 {
+	struct sigaction	test;
+
+	test.sa_handler = handler;
+	sigemptyset(&test.sa_mask);
+	test.sa_flags = SA_RESTART;
 	ft_printf("PID: %d\n", getpid());
+	sigaction(SIGUSR1, &test, NULL);
+	sigaction(SIGUSR2, &test, NULL);
 	while (1)
 	{
 		pause();
