8db31f83f161d27ca1194e7a450b695f9ade4d53
coromoEXE <corentinpapot2004@gmail.com>
2023-01-07 03:04:45 +0100

    linux added

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:32:06 by cpapot            #+#    #+#             */
-/*   Updated: 2023/01/06 16:10:24 by cpapot           ###   ########.fr       */
+/*   Updated: 2023/01/07 02:31:59 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@
 
 # include <signal.h>
 # include "../libft/includes/libft.h"
+
+# ifdef __linux__
+#  define SIGUSR1VAR	30
+#  define SIGUSR2VAR	31
+# elif __APPLE__
+#  define SIGUSR1VAR	10
+#  define SIGUSR2VAR	12
+# endif
 
 /*						Client						*/
 /*					check_error				*/
